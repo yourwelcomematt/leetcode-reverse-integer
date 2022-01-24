66c0f980915683856896433657993c47440e5672
yourwelcomematt <mattding8@gmail.com>
2022-01-24 17:30:37 +1300

    Created ReverseInteger class which reverses a given integer and returns the result, accounting for negatives and Int32 min and max

---
+++ ReverseIntegerChallenge/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReverseIntegerChallenge;
+
+var reverseInteger = new ReverseInteger();
+reverseInteger.Reverse(-123);
+reverseInteger.Reverse(456);
+reverseInteger.Reverse(789);
